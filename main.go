d397a527d3a4cfc8c8e02bf50aba4a509ce80718
Evgeni Kunev <evgeni@uber.com>
2019-01-20 11:41:57 +0200

    Return a "Hi there" in a default handler

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hiHandler(w http.ResponseWriter, r *http.Request) {
+	// Write directly to the ResponseWriter
+	fmt.Fprintf(w, "Hi there, %s!", r.URL.Path[1:])
+}
+
 func main() {
-	return
+	// Set the handler function for the root URI
+	http.HandleFunc("/", hiHandler)
+
+	// Start the server and log any error it returns. The call to
+	// http.ListenAndServe will only return when the server stops for some
+	// reason.
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
